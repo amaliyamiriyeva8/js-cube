8555c6acb208309a7a2d3862710227f82d858006
Khaticaa <mirzoevaxedice0@gmail.com>
2023-11-25 11:13:06 +0400

    js-cube

---
+++ script.js
@@ -0,0 +1,59 @@
+// let div1=document.getElementById('div1');
+// let div2=document.getElementById('div2');
+// let div3=document.getElementById('div3');
+// let div4=document.getElementById('div4');
+// let div5=document.getElementById('div5');
+
+// div1.addEventListener('click',()=>{
+//     div1.style.backgroundColor="yellow"
+//     div3.style.backgroundColor=""
+//     div4.style.backgroundColor=""
+//     div5.style.backgroundColor=""
+//     div2.style.backgroundColor=""
+// })
+// div2.addEventListener('click',()=>{
+  
+//     div1.style.backgroundColor="yellow"
+//     div2.style.backgroundColor="yellow"
+//     div3.style.backgroundColor=""
+//     div4.style.backgroundColor=""
+//     div5.style.backgroundColor=""
+// })
+// div3.addEventListener('click',()=>{
+   
+//     div1.style.backgroundColor="yellow";
+//     div2.style.backgroundColor="yellow"
+//     div3.style.backgroundColor="yellow";
+//     div4.style.backgroundColor=""
+//     div5.style.backgroundColor=""
+// })
+// div4.addEventListener('click',()=>{
+    
+//     div1.style.backgroundColor="yellow";
+//     div2.style.backgroundColor="yellow";
+//     div3.style.backgroundColor="yellow";
+//     div4.style.backgroundColor="yellow";
+//     div5.style.backgroundColor=""
+   
+// })
+// div5.addEventListener('click',()=>{
+//     div1.style.backgroundColor="yellow";
+//     div2.style.backgroundColor="yellow";
+//     div3.style.backgroundColor="yellow";
+//     div4.style.backgroundColor="yellow";
+//     div5.style.backgroundColor="yellow";
+// })
+
+// let Div=document.querySelectorAll('div');
+// Div.forEach(element => {
+//     element.addEventListener('click',(e)=>{
+//       Div.forEach(element=>{
+//         element.style.backgroundColor="";
+//       })
+//       Div.forEach(element=>{
+//         if(e.target.style.backgroundColor!=="yellow"){
+//             element.style.backgroundColor="yellow"
+//         }
+//       })
+//     })
+// });
